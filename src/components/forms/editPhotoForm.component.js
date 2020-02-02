293a5c1225ebe555ba67d8732e9458d3229d4903
srahooa <sarahoaldrich@gmail.com>
2020-02-02 17:05:14 +1000

    start of search functionality

--- src/components/forms/editPhotoForm.component.js
+++ src/components/forms/editPhotoForm.component.js
@@ -24,7 +24,7 @@ export default function EditCategoryForm(props) {
         .catch(err =>console.log(err.response.data.errorMessage))  
     }
 
-    const GetCategories = (array) => { 
+    const GetCategories = (array) => {
         setformcat(array)
     }
 
--- src/components/forms/searchForm.component.js
+++ src/components/forms/searchForm.component.js
@@ -3,18 +3,43 @@ import CategoryFilters from './categoryFilters.component'
 import '../../styles/components/forms/categoryFilters.style.scss'
 
 
-
-export default function SearchFilters() {
+export default function SearchFilters(props) {
 
    const [formcat, setformcat] = useState()
-   const all = [photos]
-   const results = []
-   const searchTerms = []
+
+   // const all = [photos]
+   const [results, setResults] = useState()
+   const [all, setAll] = useState()
+
+   const getAll = () => {
+      event.preventDefault()
+      API.get(`/photos/`)
+      .then(res => { setAll(res)} )
+      .catch(err =>console.log(err.response.data.errorMessage))
+   }
+
+   useEffect(() => {
+      getAll()
+   }, [])
+
+   const searchDescription = event => {
+      let resultsArr = []
+      event.preventDefault()
+      let searchTerms = event.target.search.value.split(' ')
+      searchTerms.forEach(term => {
+         all.forEach(photo => {
+            if (photo.description.search(term)) {
+               resultsArr.push(photo)
+            }
+         })
+      })
+      setResults(resultsArr)
+   }
+
 
    const GetCategories = (array) => {
       setformcat(array)
    }
-
 
    return (
       <>
