b11e6cc06f95c9e08032e57fc90cddd05154a44a
Roarkz <rory_bell_88@hotmail.com>
2020-02-10 16:59:35 +1000

    playing around with imgix

--- src/pages/search.page.js
+++ src/pages/search.page.js
@@ -48,7 +48,7 @@ export default function Search(props) {
             allPhotos &&
             allPhotos.map(photo => (
               <div key={photo.name} className="card">
-                <img src={`${photo.fileRef}`} alt="" />
+                <img src={`https://archivise.imgix.net/${photo.fileName}?/w=300`} alt="" />
                 <Link key={photo.id} to={{ pathname: `/photo/${photo._id}` }}>
                   {photo.name}
                 </Link>
